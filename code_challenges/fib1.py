57d8197d57ccaeef0e513a5397224b9a27ab6310
Sarah <sarah@cleancodecompany.com>
2022-06-02 20:52:36 +0100

    Tidies up Fib code challange and added three columns to Sligo Website mini project

--- code_challenges/fib1.py
+++ code_challenges/fib1.py
@@ -2,24 +2,11 @@
 # print out number at that index position
 
 
-# userIndex is the end of the range?
-
-
-#if userIndex == fib1  or userIndex == fib3  or userIndex == fib5:
-#    print(userIndex)
-# doesnt work as userIndex is an index not the actuall value!
-
-
-#  list of fib sequence
+# list of Fibonacci sequence
 fibList = [0,0,1]
 
-
 # fib logic function
-def fibo():
-    #  get user input
-    userIndex = int(input("\tWhat index do you want to view?"))
-
-    
+def fibo(userIndex):
     if userIndex == 0 or userIndex == 1:
         print("\tThe number at that position is 0")
     else:
@@ -27,5 +14,3 @@ def fibo():
             newVal = fibList[-1]+ fibList[-2]
             fibList.append(newVal)
         print("\tThe number at %d position is %d" % (userIndex, fibList[-1]))
-
-fibo()

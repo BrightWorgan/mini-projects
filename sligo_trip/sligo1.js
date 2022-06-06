9aa7930db0ecddeb32e8ba65f68585951debf127
Sarah <sarah@cleancodecompany.com>
2022-06-06 23:48:54 +0100

    added more buttons with differnt alerts

--- sligo_trip/sligo1.js
+++ sligo_trip/sligo1.js
@@ -4,14 +4,16 @@ function startJava() {
 
 
 function countyAlert() {
+  alert("County Sligo has a population of just over  90,000 people")
 
 }
 
 function cityAlert() {
-
+  alert("Sligo City is home to 1, 2, 3, 4, 5, 6, 7, 8, 9, 10")
 }
 
 
 function beachAlert() {
+  alert("There are 25 beaches in Sligo County")
 
 }

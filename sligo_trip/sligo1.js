7fec11971c8d3b542a9adbe766028f4a9289adc7
Sarah <sarah@cleancodecompany.com>
2022-06-07 16:50:13 +0100

    changed button sixe and added alert names

--- sligo_trip/sligo1.js
+++ sligo_trip/sligo1.js
@@ -15,5 +15,81 @@ function cityAlert() {
 
 function beachAlert() {
   alert("There are 25 beaches in Sligo County")
+}
 
+
+function summerhillCollege() {
+  alert("")
+}
+
+function cathedralImmCon() {
+  alert("")
+}
+
+function stJohnsChurch() {
+  alert("")
+}
+
+function presbyterianChurch() {
+  alert("")
+}
+
+function highStreet() {
+  alert("")
+}
+
+function oldMarketStreet() {
+  alert("")
+}
+
+function courthouse() {
+  alert("")
+}
+
+function sligoAbbey() {
+  alert("")
+}
+
+function river() {
+  alert("")
+}
+
+function theModel() {
+  alert("")
+}
+
+function libraryAndMuseum() {
+  alert("")
+}
+
+function ulsterBank() {
+  alert("")
+}
+
+function dHydeBridge() {
+  alert("")
+}
+
+function yeatsmemBuilding() {
+  alert("")
+}
+
+function cityHall() {
+  alert("")
+}
+
+function wineStreetAndMethodistChurch() {
+  alert("")
+}
+
+function poliexfenHouse() {
+  alert("")
+}
+
+function strandHill() {
+  alert("")
+}
+
+function rossesPoint() {
+  alert("")
 }

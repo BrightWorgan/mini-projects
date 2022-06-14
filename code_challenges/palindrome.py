b905ecc92649f0cd22d4c9c55a94ea8a4edce4ce
Sarah <sarah@cleancodecompany.com>
2022-06-14 23:39:33 +0100

    Palindrome Leet Code Complete

--- code_challenges/palindrome.py
+++ code_challenges/palindrome.py
@@ -1,11 +1,9 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if len(x) == 3:
-            return x
-        # we need to find the lenght of x
-        # need to check if first and last number are the same
-        # 0 and -1 if in a list
-        # 1 and -2 
-        # etc. 
-        # or can we reverse the number and == to x?
-        # and return true or false?
+        z = str(x)
+        for n in range(0, len(z)/2):
+            if z[n] == z[-n-1]:
+                pass
+            else:
+                return False
+        return True

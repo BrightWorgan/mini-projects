83ff405ad6a3a229e9cda0b826a21d749e83e2fd
Sarah <sarah@cleancodecompany.com>
2022-06-16 22:39:15 +0100

    progress on leet code

--- code_challenges/romanToInteger.py
+++ code_challenges/romanToInteger.py
@@ -1,5 +1,28 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        alexander = []
-        if s == "I":
-            alexander += 1
+        import re
+        alexander = 0
+        for i in s:
+            if i == "I":
+                alexander += 1
+            if i == "V":
+                alexander += 5
+            if i == "X":
+                alexander += 10
+            if i == "L":
+                alexander += 50
+            if i == "C":
+                alexander += 100
+            if i == "D":
+                alexander += 500
+            if i == "M":
+                alexander += 1000
+                # I before V and X
+                # X before L and C
+                # C before D and M
+                # if alexander == "IV""IX""XL""XC""CD""CM"
+                if re.match(" +", alexander ):
+                    return int(alexander)-1
+        return alexander
+        
+    romanToInt("III")

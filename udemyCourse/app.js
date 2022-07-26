5e7df3ac9c4019b4df63c3bf2f7aab3a9ea56b28
Sarah <sarah@cleancodecompany.com>
2022-07-26 21:15:25 +0100

    added a html based blog with two posts

--- udemyCourse/app.js
+++ udemyCourse/app.js
@@ -1,5 +1,15 @@
-const pBrowser = document.querySelector('p')
-const pD3 = d3.select('p')
+// selection class
+//const pBrowser = document.querySelector('p')
+//const pD3 = d3.select('p')
 
-console.log(pBrowser)
-console.log(pD3)
+//console.log(pBrowser)
+//console.log(pD3)
+
+// transformation class
+
+// classes and styles class
+const el = d3.select('body')
+  .append('p')
+  .text('Hello Wordld')
+
+// joining data class

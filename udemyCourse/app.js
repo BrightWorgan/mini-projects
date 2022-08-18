25ec1a9357a9476d4ed46a9d42dd3990dc3171fe
Sarah <sarah@cleancodecompany.com>
2022-08-18 17:36:09 +0100

    Section 3, Part 16

--- udemyCourse/app.js
+++ udemyCourse/app.js
@@ -8,8 +8,24 @@
 // transformation class
 
 // classes and styles class
-const el = d3.select('body')
-  .append('p')
-  .text('Hello Wordld')
+//const el = d3.select('body')
+//  .append('p')
+//.attr('class', 'foo')
+//.text('Hello Wordld')
 
 // joining data class
+
+//const el = d3.select('body')
+//  .append('p')
+//.classed('foo', true)
+//  .classed('bar', true)
+//  .text('Hello World Third Text Sample')
+//  .style('color', 'blue')
+
+const data = [10, 20, 30, 40, 50]
+const el = d3.selectA('ul')
+  .selectAll('li')
+  .data(data)
+  .join('li')
+  .text('hello')
+consoile.log(el)

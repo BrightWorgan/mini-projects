b4e9ba7de58293d5d48350fcf16d33ac67f678e4
Sarah <sarah@cleancodecompany.com>
2022-08-18 18:10:09 +0100

    section 3 complete

--- udemyCourse/secondWorkSheet/app.js
+++ udemyCourse/secondWorkSheet/app.js
@@ -1,5 +1,9 @@
 function getData() {
-  const data = await d3.json('data.json')
+  // getting data from JSON file
+  //const data = await d3.json('data.json')
+  // getting data from CSV file
+  const data = await d3.csv('data.csv')
+
   console.log(data)
 }
 

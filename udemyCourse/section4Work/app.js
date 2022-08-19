c0397b02963877cf966601163ef1ffa4a7e917f2
Sarah <sarah@cleancodecompany.com>
2022-08-19 17:07:59 +0100

    part 24 complete

--- udemyCourse/section4Work/app.js
+++ udemyCourse/section4Work/app.js
@@ -6,7 +6,13 @@ async function draw() {
   // dimension of scatter graph
   let dimensions = {
     width: 800,
-    height: 800
+    height: 800,
+    margin: {
+      top: 50,
+      bottom: 50,
+      left: 50,
+      right: 50
+    }
   }
 
   // draw image
@@ -14,6 +20,16 @@ async function draw() {
     .append('svg')
     .attr('width', dimensions.width)
     .attr('height', dimensions.height)
+
+  const ctr = svg.append('g')
+    .attr(
+      'transform',
+      `translate(${dimensions.margin.left}, ${dimensions.margin.top})`
+    )
+
+
+  ctr.append('circle')
+    .attr('r', 15)
 }
 
 

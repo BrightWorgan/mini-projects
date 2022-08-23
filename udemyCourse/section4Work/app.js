4f3a83cfc0d348b89d3416cb7b95c2832a00a6e3
Sarah <sarah@cleancodecompany.com>
2022-08-23 16:42:22 +0100

    part 25 complete

--- udemyCourse/section4Work/app.js
+++ udemyCourse/section4Work/app.js
@@ -2,40 +2,48 @@ async function draw() {
   // data
   const dataset = await d3.json('data.json')
 
+  const xAccessor = (d) => d.currently.humidity
+  const yAccessor = (d) => d.currently.apparentTemperature
+}
 
-  // dimension of scatter graph
-  let dimensions = {
-    width: 800,
-    height: 800,
-    margin: {
-      top: 50,
-      bottom: 50,
-      left: 50,
-      right: 50
-    }
+
+// dimension of scatter graph
+let dimensions = {
+  width: 800,
+  height: 800,
+  margin: {
+    top: 50,
+    bottom: 50,
+    left: 50,
+    right: 50
   }
+}
 
-  // draw image
-  const svg = d3.select('#chart')
-    .append('svg')
-    .attr('width', dimensions.width)
-    .attr('height', dimensions.height)
+// draw image
+const svg = d3.select('#chart')
+  .append('svg')
+  .attr('width', dimensions.width)
+  .attr('height', dimensions.height)
 
-  const ctr = svg.append('g')
-    .attr(
-      'transform',
-      `translate(${dimensions.margin.left}, ${dimensions.margin.top})`
-    )
+const ctr = svg.append('g')
+  .attr(
+    'transform',
+    `translate(${dimensions.margin.left}, ${dimensions.margin.top})`
+  )
 
-  // Commented out first test circle
-  //ctr.append('circle')
-  // .attr('r', 15)
+// Commented out first test circle
+//ctr.append('circle')
+// .attr('r', 15)
 
-  // draw circles
-  ctr.selectAll('circle')
-    .data(dataset)
-    .join('circle')
-    .attr('cx', () => )
+// draw circles
+ctr.selectAll('circle')
+  .data(dataset)
+  .join('circle')
+  //.attr('cx', (d) => console.log(d))
+  .attr('cx', (d) => d.currently.humidity)
+  .attr('cy', (d) => d.currently.apparentTemperature)
+  .attr('r', 5)
+  .attr('fill', 'red')
 }
 
 

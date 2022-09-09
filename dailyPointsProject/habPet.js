395c76f63da8151f0994c7dd197cec77782aa010
Sarah <sarah@cleancodecompany.com>
2022-09-09 17:36:35 +0100

    added correct JS funstions to buttons and changed text

--- dailyPointsProject/habPet.js
+++ dailyPointsProject/habPet.js
@@ -19,3 +19,23 @@ function onehrEx() {
 function congratsButton() {
   alert("WELL DONE !!!!!! ")
 }
+
+function onePtBtn() {
+  alert(" Nice, that's another more point added to your total!")
+}
+
+function twoPtBtn() {
+  alert(" Congrats, that's two more points!")
+}
+
+function threePtBtn() {
+  alert(" Well done! That is three more points added to your total.")
+}
+
+function fourPtBtn() {
+  alert(" Wow! Four points added")
+}
+
+function fivePtBtn() {
+  alert(" Amazing! You added five points to your total! ")
+}

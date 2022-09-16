0c881663e887d3cd56f0a130ed7e088f44da2b4b
Sarah <sarah@cleancodecompany.com>
2022-09-16 21:27:30 +0100

    added a Class

--- taxiProject/taxiProject.py
+++ taxiProject/taxiProject.py
@@ -16,17 +16,23 @@ dic = {
         },
 }
 
-# get taxi id/ reg. number
-# userInput
+# list of question names
+listOfQuestions = ["q1", "q2", "q3", "q4","q5"]
 
-userInput = input("\tHi there\n\t Thank you for choosing to use GoTaxi\n\tPlease enter the reg number of the taxi: ").lower()
+# get taxi id/ reg. number using userInput
+userInput = input("\tPlease enter the reg number of the taxi: ").lower()
 
 
 # check if taxt id/reg. number is alrady in the database
 if userInput in dic:
-    pass
+    print("\tTaxi all ready exists, \n\t TEST Statement")
+    # update nested dict.
+    # questionSeeker()
+
 else:
-    dic['taxiID2345E'] = {"q1":"3","q2" : "3","q3" : "1","q4" : "3","q5" : "n"}
+    dic['taxiID2345E'] = {"q1":"3", "q2" : "3", "q3" : "1", "q4" : "3", "q5" : "n"}
+    # for loop
+
 
 # if present update
 
@@ -34,20 +40,38 @@ else:
 dic['taxiID2345E'] = {"q1":"3","q2" : "3","q3" : "1","q4" : "3","q5" : "n"}
 
 # five quries
-
-# 1 cleanliness
-
-# 2 General customer service
-
-# 3 Communication
-
-# 4 Driving competency
-
-# 5 yes / no question
-qwertty = input("\tDid the diver refuse service?\n y/n?\n").lower()
-
 # add number to taxi's dictionalry
-
 # calculate new average 
-
 # if updated average is below 12 generate email E1 or if nameOfQuestion is yes gernerate email E2
+
+# taxi class
+class Taxi :
+    def __init__(self, num = "", cleanRating = 0, serviceRating = 0, commRating = 0, drivingRating = 0 ):
+        # num
+        if num == None: 
+            raise Exception ("\t*** A Vaild ISBN is necessary ***")
+        self.__num = num
+
+        # q1 cleanliness 
+        if type(cleanRating) == int and cleanRating > 0 and cleanRating <= 5:
+            self.__rating = cleanRating
+        else:
+            raise Exception("*** Invaid Rating!*** Rating must be from 1-5")
+
+        # q2 General customer service
+        if type(serviceRating) == int and serviceRating > 0 and serviceRating <= 5:
+            self.__rating = serviceRating
+        else:
+            raise Exception("*** Invaid Rating!*** Rating must be from 1-5")
+        
+        # q3 Communication
+        if type(commRating) == int and commRating > 0 and commRating <= 5:
+            self.__rating = commRating
+        else:
+            raise Exception("*** Invaid Rating!*** Rating must be from 1-5")
+
+        # q4 Driving competency
+        if type(drivingRating) == int and drivingRating > 0 and drivingRating <= 5:
+            self.__rating = drivingRating
+        else:
+            raise Exception("*** Invaid Rating!*** Rating must be from 1-5")
--- taxiProject/work.js
+++ taxiProject/work.js
@@ -1,3 +1,7 @@
 function startJava() {
 
 }
+
+function greeting1() {
+  alert("\tHi there, \t\nThank you for choosing to use GoTaxi")
+}

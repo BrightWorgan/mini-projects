e4d5fa0ed848979dd35b344c17a439dc78614ec9
Sarah <sarah@cleancodecompany.com>
2022-12-07 22:08:23 +0000

    day 3 chall start

---
+++ code_challenges/adventOfCodeChallenges/day3.py
@@ -0,0 +1,8 @@
+#
+
+   # Lowercase item types a through z have priorities 1 through 26.
+    #Uppercase item types A through Z have priorities 27 through 52.
+
+# list 
+# for loop throught list and assign p?
+# all text

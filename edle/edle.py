ad707642d84e99d11a3f25d8bc6825e84958054f
Sarah <sarah@cleancodecompany.com>
2022-12-21 21:14:23 +0000

    start of city escape website

--- edle/edle.py
+++ edle/edle.py
@@ -19,32 +19,39 @@
 
 
 # list of words to be removed
-RemovalWords = []
+RemovalWords = ["the", "and", "a", "in", "out", "up", "down", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "where", "when", "there", "their", "you", "your", "who", "what", "you're", "that", "second", "minute", "hour"]
 
 # play = "n"
 # get user input
-play = input("\tHello there. Do you wish to play a game? \n\t Y/N?").lower()
-# while loop
-while play != "y":
-# get user input
-    # play = input("\tHello there. Do you wish to play a game? \n\t Y/N?").lower()
-    # check user's answer
-    if play == "y":
-        print("\tFantastic! The let's get started")
-    elif play == "n":
-        print("\tMaybe another time.")
-    else: 
-        print("\t ERROR: Please enter Y or N")
+# play = input("\tHello there. Do you wish to play a game? \n\t Y/N?").lower()
+# # while loop
+# while play != "y":
+# # get user input
+#     # play = input("\tHello there. Do you wish to play a game? \n\t Y/N?").lower()
+#     # check user's answer
+#     if play == "y":
+#         print("\tFantastic! The let's get started")
+#     elif play == "n":
+#         print("\tMaybe another time.")
+#     else: 
+#         print("\t ERROR: Please enter Y or N")
 
 # get user's paragraph
 paragraph = input("\tPlease enter the paragraph you wish to Edle today! ")
 
 # list of word
-booklet = list.spilt(paragraph)
+booklet = paragraph.split()
 print(booklet)
+prop = []
 # remove "and" words
-for word in len(booklet):
-    if word  in RemovalWords:
-        booklet.remove(word)
+for word in range(0, len(booklet)):
+    if word not in  RemovalWords:
+        prop.append(word)
+
+print(booklet)
+print(prop)
+
+# for i in range(len(prop)):
+#     print(prop[i])
 
 
